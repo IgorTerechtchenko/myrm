fb98bdb2bc6b809070e1725d752d705ba4039d61
Igor Tereschenko <pokawork@gmail.com>
2017-04-05 17:42:46 +0300

    deletefile and deletedir redone

--- 2/myrm/deleter.py
+++ 2/myrm/deleter.py
@@ -3,6 +3,8 @@
 import shutil
 import os
 
+# TODO separate filenames from paths
+
 
 class Deleter(object):
     def __init__(self, re_bin_location=os.environ['HOME'] + '/recycle_bin',
@@ -15,26 +17,37 @@ class Deleter(object):
         self.options = options
 
     def deletedir(self, dirname):
-        if dirname in os.listdir(self.re_bin_location):
+        try:
+            shutil.move(dirname, self.re_bin_location + '/')
+        except shutil.Error:
             if self.options['same_name_confres'] is 'replace':
-                shutil.copytree(dirname, self.re_bin_location + '/')
+                shutil.rmtree(self.re_bin_location + '/' + dirname)
+                shutil.copytree(dirname, self.re_bin_location + '/' + dirname)
                 shutil.rmtree(dirname)
             elif self.options['same_name_confres'] is 'keep':
-                print 'file with such name already exists in the recycle bin'
-        else:
-            shutil.move(dirname, self.re_bin_location + '/')
+                print 'directory with name "{}" already exists in the recycle bin'.format(dirname)
 
     def deletefile(self, filename):
-        if filename in os.listdir(self.re_bin_location):
-            if self.options['same_name_confres'] is 'replace':
-                shutil.copy(filename, self.re_bin_location + '/')
-                os.remove(filename)
-            elif self.options['same_name_confres'] is 'keep':
-                print 'file with name "{}" already exists in the recycle bin'.format(filename)
-        else:
-            shutil.move(filename, self.re_bin_location + '/')
+        operation_results = []
+        try:
+            try:
+                shutil.move(filename, self.re_bin_location + '/')
+                operation_results.append('file {} moved to {}'.format(filename, self.re_bin_location))
+            except shutil.Error:
+                if self.options['same_name_confres'] is 'replace':
+                    shutil.copy(filename, self.re_bin_location + '/')
+                    os.remove(filename)
+                    operation_results.append('file {} moved to {}'.format(filename, self.re_bin_location))
+                elif self.options['same_name_confres'] is 'keep':
+                    print 'file with name "{}" already exists in {}\naborting'.format(filename, self.re_bin_location)
+        except IOError:
+            print 'no file named "{}" located'.format(filename)
+        if self.options['output'] is 'verbose' and len(operation_results) > 0:
+            for i in operation_results:
+                print i
 
 
-o = {'same_name_confres': 'keep'}
+o = {'same_name_confres': 'replace',
+     'output': 'verbose'}
 D = Deleter(options=o)
-D.deletefile('a')
+D.deletedir('dir')
