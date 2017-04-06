c960a5c8299e22b9a212ca644ab61d56b02719cf
Igor Tereschenko <pokawork@gmail.com>
2017-04-06 19:40:07 +0300

    Did some cleanup and implemented general delete method.

--- 2/myrm/deleter.py
+++ 2/myrm/deleter.py
@@ -16,51 +16,57 @@ class Deleter(object):
             'using bin at {}'.format(re_bin_location)
         self.options = options
 
-    def deletedir(self, dirname):
-        operation_results = []
-        try:
-            try:
-                shutil.move(dirname, self.re_bin_location + '/')
-                operation_results.append('dir "{}" with all contents moved to {}'.format(dirname, self.re_bin_location))
-            except shutil.Error:
-                if self.options['replace_same_name'] is True:
-                    shutil.rmtree(self.re_bin_location + '/' + dirname)
-                    shutil.copytree(dirname, self.re_bin_location + '/' + dirname)
-                    shutil.rmtree(dirname)
-                    operation_results.append('dir "{}" with all contents moved to {}'.format(dirname, self.re_bin_location))
-                elif self.options['replace_same_name'] is 'keep':
-                    print 'directory with name "{}" already exists in the recycle bin'.format(dirname)
-        except IOError:
-            print 'no file dir "{}" located'.format(dirname)
+    def report_results(self, operation_results):
         if self.options['output'] is 'verbose' and len(operation_results) > 0:
             for i in operation_results:
                 print i
+
+    def deletedir(self, dirname):
+        print 'dir'
+        operation_results = []
+        try:
+            shutil.move(dirname, self.re_bin_location + '/')
+            operation_results.append('dir "{}" with all contents moved to {}'.format(dirname, self.re_bin_location))
+        except shutil.Error:
+            if self.options['replace_same_name'] is True:
+                shutil.rmtree(self.re_bin_location + '/' + dirname)
+                shutil.copytree(dirname, self.re_bin_location + '/' + dirname)
+                shutil.rmtree(dirname)
+                operation_results.append('dir "{}" with all contents moved to {}'.format(dirname, self.re_bin_location))
+            elif self.options['replace_same_name'] is False:
+                print 'directory with name "{}" already exists in the recycle bin'.format(dirname)
+        self.report_results(operation_results)
         return operation_results
 
     def deletefile(self, filename):
         operation_results = []
         try:
-            try:
-                shutil.move(filename, self.re_bin_location + '/')
+            shutil.move(filename, self.re_bin_location + '/')
+            operation_results.append('file "{}" moved to {}'.format(filename, self.re_bin_location))
+        except shutil.Error:
+            if self.options['replace_same_name'] is True:
+                shutil.copy(filename, self.re_bin_location + '/')
+                os.remove(filename)
                 operation_results.append('file "{}" moved to {}'.format(filename, self.re_bin_location))
-            except shutil.Error:
-                if self.options['replace_same_name'] is True:
-                    shutil.copy(filename, self.re_bin_location + '/')
-                    os.remove(filename)
-                    operation_results.append('file "{}" moved to {}'.format(filename, self.re_bin_location))
-                elif self.options['replace_same_name'] is 'keep':
-                    print 'file with name "{}" already exists in {}\naborting'.format(filename, self.re_bin_location)
-        except IOError:
-            print 'no file named "{}" located'.format(filename)
-        if self.options['output'] is 'verbose' and len(operation_results) > 0:
-            for i in operation_results:
-                print i
+            elif self.options['replace_same_name'] is False:
+                print 'file with name "{}" already exists in {}\naborting'.format(filename, self.re_bin_location)
+        self.report_results(operation_results)
         return operation_results
+
+    def delete(self, object_name):  # wrapper that unites all delete methods and handles errors
+        try:
+            if os.path.isfile(object_name):
+                self.deletefile(object_name)
+            else:
+                self.deletedir(object_name)
+        except OSError:
+            print 'no file or directory named "{}"'.format(object_name)
 
 
 o = {'replace_same_name': True,
      'output': 'verbose'}
 D = Deleter(options=o)
-D.deletedir('dir')
-D.deletefile('a')
-D.deletefile('b')
+D.delete('dir')
+D.delete('a')
+D.delete('b')
+D.delete('c')
