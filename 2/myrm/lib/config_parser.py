ad566e27560927c16b319714def4de7a1ef6fd43
Igor Tereschenko <pokawork@gmail.com>
2017-04-06 22:53:12 +0300

    bin added

--- 2/myrm/lib/config_parser.py
+++ 2/myrm/lib/config_parser.py
@@ -1,14 +1,18 @@
 import json
+import sys
 
 
 class ConfigParser(object):
     def __init__(self, config_file_path):
         try:
             f = open(config_file_path, 'r')
+            self.options_dict = json.loads(f.read())
         except IOError:
             print 'no file named "{}"'.format(config_file_path)
-        self.options_dict = json.loads(f.read())
-        print self.options_dict
 
-
-cp = ConfigParser('config')
+    def get_options(self):
+        try:
+            return self.options_dict
+        except NameError:
+            print 'no optoins parsed'
+            sys.exit()
